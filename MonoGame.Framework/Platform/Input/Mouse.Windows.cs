91ef5de8ebad4238913fc67559ebb8fd4619288e
Harm Wind <harm.wind@vanoord.com>
2024-02-21 11:55:48 +0100

    Fix Mouse PlatformGetState method for use in Windows Forms

--- MonoGame.Framework/Platform/Input/Mouse.Windows.cs
+++ MonoGame.Framework/Platform/Input/Mouse.Windows.cs
@@ -10,39 +10,128 @@ namespace Microsoft.Xna.Framework.Input
 {
     public static partial class Mouse
     {
-        [DllImportAttribute("user32.dll", EntryPoint = "SetCursorPos")]
-        [return: MarshalAsAttribute(System.Runtime.InteropServices.UnmanagedType.Bool)]
+        [StructLayout(LayoutKind.Sequential)]
+        internal struct POINTSTRUCT
+        {
+            public int X;
+            public int Y;
+        }
+
+        [DllImport("user32.dll", EntryPoint = "SetCursorPos")]
+        [return: MarshalAs(UnmanagedType.Bool)]
         private static extern bool SetCursorPos(int X, int Y);
 
-        private static Control _window;
+        [DllImport("user32.dll", ExactSpelling = true, CharSet = CharSet.Auto)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        internal static extern bool GetCursorPos(out POINTSTRUCT pt);
+
+        [DllImport("user32.dll", ExactSpelling = true, CharSet = CharSet.Auto)]
+        internal static extern int MapWindowPoints(IntPtr hWndFrom, IntPtr hWndTo, out POINTSTRUCT pt, int cPoints);
+
+        [DllImport("user32.dll")]
+        private static extern short GetAsyncKeyState(int key);
+
+        private static IntPtr _windowHandle;
+        private static MouseInputWnd _mouseInputWnd = new MouseInputWnd();
 
         private static IntPtr PlatformGetWindowHandle()
         {
-            return _window.Handle;
+            return _windowHandle;
         }
 
         private static void PlatformSetWindowHandle(IntPtr windowHandle)
         {
-            _window = Control.FromHandle(windowHandle);
+            // Release old Handle
+            if (_mouseInputWnd.Handle != IntPtr.Zero)
+                _mouseInputWnd.ReleaseHandle();
+
+            _windowHandle = windowHandle;
+            _mouseInputWnd.AssignHandle(windowHandle);
         }
 
         private static MouseState PlatformGetState(GameWindow window)
         {
-            return window.MouseState;
+            if (_windowHandle != IntPtr.Zero)
+            {
+                POINTSTRUCT pos;
+                GetCursorPos(out pos);
+                MapWindowPoints(IntPtr.Zero, _windowHandle, out pos, 1);
+                var clientPos = new System.Drawing.Point(pos.X, pos.Y);
+
+                //Use GetAsyncKeyState instead of Control.MouseButtons since this also works from other threads than the main thread
+
+                bool leftButton = GetAsyncKeyState(0x01) < 0;
+                bool rightButton = GetAsyncKeyState(0x02) < 0;
+                bool middleButton = GetAsyncKeyState(0x04) < 0;
+                bool xButton1 = GetAsyncKeyState(0x05) < 0;
+                bool xButton2 = GetAsyncKeyState(0x06) < 0;
+
+                return new MouseState(
+                    clientPos.X,
+                    clientPos.Y,
+                    _mouseInputWnd.ScrollWheelValue,
+                    leftButton ? ButtonState.Pressed : ButtonState.Released,
+                    middleButton ? ButtonState.Pressed : ButtonState.Released,
+                    rightButton ? ButtonState.Pressed : ButtonState.Released,
+                    xButton1 ? ButtonState.Pressed : ButtonState.Released,
+                    xButton2 ? ButtonState.Pressed : ButtonState.Released,
+                    _mouseInputWnd.HorizontalScrollWheelValue
+                    );
+            }
+
+            return _defaultState;
         }
 
         private static void PlatformSetPosition(int x, int y)
         {
-            PrimaryWindow.MouseState.X = x;
-            PrimaryWindow.MouseState.Y = y;
-            
-            var pt = _window.PointToScreen(new System.Drawing.Point(x, y));
-            SetCursorPos(pt.X, pt.Y);
+            if (PrimaryWindow != null)
+            {
+                PrimaryWindow.MouseState.X = x;
+                PrimaryWindow.MouseState.Y = y;
+            }
+
+            SetCursorPos(x, y);
+
+            //var pt = _window.PointToScreen(new System.Drawing.Point(x, y));
+            //SetCursorPos(pt.X, pt.Y);
         }
 
         public static void PlatformSetCursor(MouseCursor cursor)
         {
-            _window.Cursor = cursor.Cursor;
+            //_window.Cursor = cursor.Cursor;
         }
+
+        #region Nested class MouseInputWnd
+
+        /// <remarks>
+        /// Subclass WindowHandle to read WM_MOUSEWHEEL and WM_MOUSEHWHEEL messages
+        /// </remarks>
+        class MouseInputWnd : NativeWindow
+        {
+            const int WM_MOUSEWHEEL = 0x020A;
+            const int WM_MOUSEHWHEEL = 0x020E;
+
+            public int ScrollWheelValue = 0;
+            public int HorizontalScrollWheelValue = 0;
+
+            protected override void WndProc(ref Message m)
+            {
+                switch (m.Msg)
+                {
+                    case WM_MOUSEWHEEL:
+                        var delta = (short)(((ulong)m.WParam >> 16) & 0xffff);
+                        ScrollWheelValue += delta;
+                        break;
+                    case WM_MOUSEHWHEEL:
+                        var deltaH = (short)(((ulong)m.WParam >> 16) & 0xffff);
+                        HorizontalScrollWheelValue += deltaH;
+                        break;
+                }
+
+                base.WndProc(ref m);
+            }
+        }
+
+        #endregion Nested class MouseInputWnd
     }
 }
